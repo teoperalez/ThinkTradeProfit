29d6277c77259dbfab53a2e4f66d944fb1770ec6
teoperalez <93870128+teoperalez@users.noreply.github.com>
2022-01-02 01:49:12 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,27 @@ import VideoList from '../Components/VideoList'
 import StratList from '../Components/StratList'
 import BlogList from '../Components/BlogList'
 import Robinhood from '../Components/Robinhood'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 
-export default function Home({articles, indicators}) {
+export default function Home() {
   const [section, setSection] = useState(1); 
-  
+  const [indicators, setIndicators] = useState([]);
 
+  useEffect( () =>  {
+    
+    const fetchData = async () => {
+    const response = await fetch("http://localhost:3000/api/indicator");
+
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status}`);
+    };
+    const indi = await response.json();
+    setIndicators(indi)
+    };
+  
+    fetchData();
+  }, []);
 
   return (
     <div>
