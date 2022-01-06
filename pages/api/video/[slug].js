ab725ac688cfa1b3724e9dbc6ec3609dba5804f1
teoperalez <93870128+teoperalez@users.noreply.github.com>
2022-01-07 00:36:35 +0900

    many updates

--- pages/api/video/[slug].js
+++ pages/api/video/[slug].js
@@ -4,9 +4,9 @@ export default function handler(request, response) {
   const { method } = request;
 
   if (method === "GET") {
-    const { id } = request.query;
+    const { slug } = request.query;
 
-    const indi = data.find((indi) => indi.id.toString() === id);
+    const indi = data.find((indi) => indi.slug.toString() === slug);
 
     if (!indi) {
       return response.status(400).json("Indicator not found");
