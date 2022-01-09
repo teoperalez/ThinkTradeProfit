df8019868f729968c136dbc70e23d159413d5539
teoperalez <93870128+teoperalez@users.noreply.github.com>
2022-01-09 15:43:03 +0900

    try 2

--- components/BLayout.js
+++ components/BLayout.js
@@ -4,7 +4,9 @@ export default function BLayout({ preview, children }) {
   return (
     <>
       <PageMeta title="Trading ThinkScripts || Top" description="Strategies for the Intelligent Trader" tags="investing, forex, stocks, techncal analysis, charts, trading signals, trading indicators, backtesting" /> 
-      
+      <div className="min-h-screen">
+        <main>{children}</main>
+      </div>
     </>
   )
 }
