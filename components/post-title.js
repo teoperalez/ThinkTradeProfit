327dd519c52d5d6b424bd71f2d41268b7ac93ed1
teoperalez <93870128+teoperalez@users.noreply.github.com>
2022-01-09 10:32:45 +0900

    Update post-title.js

--- components/post-title.js
+++ components/post-title.js
@@ -2,6 +2,7 @@ export default function PostTitle({ children }) {
   return (
     <h1 className="text-6xl md:text-7xl lg:text-8xl font-bold tracking-tighter leading-tight md:leading-none mb-12 text-center md:text-left">
       {children}
-    </h1>
+    </h1>   
+  
   )
 }
