53bfd5a1086a4aa95f0edd40d6e023c7f0a051df
teoperalez <93870128+teoperalez@users.noreply.github.com>
2022-01-09 15:23:30 +0900

    try 3

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import TutorialsList from '../components/tutorialslist';
 import ThoughtList from '../components/thoughtlist';
 import BlockFi from '../components/BlockFi';
 import Robinhood from '../components/robinhood';
+import IList from '../components/IList';
 
 
 export default function Home() {
@@ -13,7 +14,7 @@ export default function Home() {
       <PageMeta title="Trading ThinkScripts || Top" description="Strategies for the Intelligent Trader" tags="investing, forex, stocks, techncal analysis, charts, trading signals, trading indicators, backtesting" /> 
       <ThoughtList />
       <BlockFi />
-      
+      <IList />
       <Robinhood />
       <TutorialsList />
       
