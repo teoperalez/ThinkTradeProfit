edc6621ebad26aaf1973a771581db900de15466d
teoperalez <93870128+teoperalez@users.noreply.github.com>
2022-01-09 15:24:52 +0900

    try 4

--- components/IList.js
+++ components/IList.js
@@ -7,7 +7,29 @@ const IList = ({indicators}) => {
     
     return (
         <div className={indiStyles.sectiontop}>
-            
+            <h1 className={indiStyles.title}>Indicators</h1>
+            <div className={indiStyles.contents}>
+            {indicators.map((indicator, index) => (
+              <div key ={index} id ={indicator.id}  className={indiStyles.card}  >
+                  <div className={indiStyles.img} > 
+              <Image src={`/${indicator.image}`} layout="fill" objectFit="cover"/>
+              </div>
+              <h2 className={indiStyles.cardtitle}>{indicator.title}</h2>
+              <div className={indiStyles.cardcontent}>
+                
+              
+              <p className={indiStyles.cardbody}>{indicator.description}</p>
+              <Link
+                href="/indicator/[id]"
+                as={`/indicator/${indicator.id}`}
+                passHref
+              >
+                  <a className={indiStyles.button}>Read More...</a>
+              </Link>
+          </div>
+          </div>  
+            ))}
+            </div>
         </div>
     );
 };
