e10077bdde0a921fd0674a404ae6392940d03310
teoperalez <93870128+teoperalez@users.noreply.github.com>
2022-01-11 02:33:52 +0900

    try

--- viddata.js
+++ viddata.js
@@ -5,7 +5,7 @@ export const data = [
       slug: "Zc4_kujRS-Y",
       list: "tutorial",
       description: "In today's tutorial, we will learn how to make a chart significantly more readable by adding colored bars associated with different conditions – Overbought, Oversold, and Middle Line Crossovers for entry signals.",
-      image: "/assets/blog/preview/Cover.jpg",
+      image: "/assets/blog/preview/cover.jpg",
       tags: "ThinkorSwim, ThinkScript, MACD, RSI, Bollinger Bands, Exponential Moving Average, EMA, Technical Analysis, Study, Finance, Investing, Profit, Youtube",
       url: "https://youtu.be/Zc4_kujRS-Y",
       body: "tutorial_1",
