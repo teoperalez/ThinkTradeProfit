bfa4d79959c0a3fa3cad44e3ee2c21a4755eb8d4
teoperalez <93870128+teoperalez@users.noreply.github.com>
2022-01-25 00:59:24 +0900

    try

--- pages/video/index.js
+++ pages/video/index.js
@@ -6,7 +6,7 @@ export default function Video({ videos }) {
     return (
         <>
     <BLayout>
-    <PageMeta title="ThinkTradeProfit.com || Thoughts Blog" description="Strategies for the Intelligent Trader" tags="investing, forex, stocks, techncal analysis, charts, trading signals, trading indicators, backtesting" /> 
+    <PageMeta title="ThinkTradeProfit.com || Videos" description="Strategies for the Intelligent Trader" tags="investing, forex, stocks, techncal analysis, charts, trading signals, trading indicators, backtesting" /> 
          <VideoList videos={videos}/>         
     </BLayout>
       
--- viddata.js
+++ viddata.js
@@ -10,6 +10,18 @@ export const data = [
       url: "https://youtu.be/Zc4_kujRS-Y",
       body: "tutorial_1",
     },
+
+    {
+      id: 2,
+      title: "Full Backtest: RSI + 200EMA VS RSI + BB + 200EMA on #SPY, /#BTC, & #EUR/USD #Backtests",
+      slug: "w8mcKLcR3_M",
+      list: "backtest",
+      description: "Today, we have a full backtest of the RSI + 200EMA Strategy and the RSI + Bollinger Bands + 200EMA Strategy.   Here are the rules: For the RSI Strategy, we needed to be above the 200EMA for long entries.   Then, we would buy as soon as the RSI became oversold with a 1 ATR Profit Target and a 1.5 ATR Stoploss. For short entries, we entered when price was below the 200EMa and the RSI crossed into overbought territory.  For the Combined RSI and Bollinger Bands Strategy, we used the 200EMA for trend direction as well.   This time, we took long entries when the RSI crossed below oversold AND the close was less than the lower Bollinger Band with a 1 ATR Profit Target and a 1.5 ATR Stoploss.    For Short entries, we looked for the RSI to cross into overbought territory and the close to be above the upper Bollinger Band. Settings:  #RSI: periods, Overbought Level 30, Oversold Level 70.  #BollingerBands: 20-periods +- 2 Standard Deviations #ATR: 14-periods, Wilder Average Stoploss: 1.5 ATR Take Profit: 1ATR Break Even Winrate: 60%",
+      image: "/videos/RSIBBBacktestCover.jpg",
+      tags: "ThinkorSwim, ThinkScript, RSI, Bollinger Bands, Backtest, Exponential Moving Average, EMA, Technical Analysis, Study, Finance, Investing, Profit, Youtube",
+      url: "https://www.youtube.com/watch?v=w8mcKLcR3_M",
+      body: "Backtest_1",
+    },
     
    
   ];
